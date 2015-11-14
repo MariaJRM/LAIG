b2029c33f6255da33c72336cc9aee094ae67c368
Sofia Reis <sofiareis1994@gmail.com>
2015-11-14 17:38:42 +0000

    PATCH DONE! eheheh
    -> Need some code optimization; (nothing special)
    -> added bb-8 made and textures made for Maria

--- P2/reader/primitives/Patch.js
+++ P2/reader/primitives/Patch.js
@@ -1,11 +1,11 @@
 function createKnot(order){
-	var i = Math.pow((order+1),2);
+	var i = order+1;
 	var knot = new Array(i);
-	var middle = i/2;
+	var n = i*2;
 
-	for(var j = 0; j < i;j++)
+	for(var j = 0; j < n;j++)
 	{
-		if(j < middle)
+		if(j < i)
 			knot[j] = 0;
 		else
 			knot[j] = 1;
@@ -22,7 +22,7 @@ function Patch(scene, order, partsU, partsV, c_points) {
 
 	var controlVertexes = [];
 
-	// É preciso otimizar isto!!!
+	// É preciso otimizar isto!!! Está a rebentar porque isto está mal feito eheheheh
 	if(order == 1){
 		controlVertexes[0] = [c_points[0],c_points[1]];
 		controlVertexes[1] = [c_points[2],c_points[3]];
@@ -34,17 +34,21 @@ function Patch(scene, order, partsU, partsV, c_points) {
 		controlVertexes[2] = [c_points[6],c_points[7], c_points[8]];
 	}
 	else if(order == 3){
+	
 		controlVertexes[0] = [c_points[0],c_points[1], c_points[2] , c_points[3]];
+			console.log(controlVertexes[0]);
 		controlVertexes[1] = [c_points[4],c_points[5], c_points[6], c_points[7]];
 		controlVertexes[2] = [c_points[8],c_points[9], c_points[10] , c_points[11]];
-		controlVertexes[3] = [c_points[12],c_points[12], c_points[14], c_points[15]];
+		controlVertexes[3] = [c_points[12],c_points[13], c_points[14], c_points[15]];
 	}
 
-	//console.log(controlVertexes);
+	console.log(controlVertexes);
+
+	console.log(order);
+	console.log(knots);
 
 	var nurbsSurface = new CGFnurbsSurface(order, order, 
-		knots, knots, 
-				controlVertexes);
+		knots, knots, controlVertexes);
 
 	getSurfacePoint = function(u, v) {
 		return nurbsSurface.getPoint(u, v);
