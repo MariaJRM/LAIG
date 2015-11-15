f39f2e0bd16a10f7c996670536554a67599c0a31
Sofia Reis <sofiareis1994@gmail.com>
2015-11-15 03:45:23 +0000

    functions for vehicle created

--- P2/reader/primitives/Vehicle.js
+++ P2/reader/primitives/Vehicle.js
@@ -1,1 +1,15 @@
-// CRIAR VEICULO QUE VOA COM PATCH :)
+function Vehicle(scene) {
+
+	CGFobject.call(this,scene);
+
+	
+	this.initBuffers();
+};
+
+Vehicle.prototype = Object.create(CGFobject.prototype);
+Vehicle.prototype.constructor=Vehicle;
+
+Vehicle.prototype.display = function() {
+
+
+}
