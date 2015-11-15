17aad92dabb9e46443d1a7ba1aef4defe553dc8f
Sofia Reis <sofiareis1994@gmail.com>
2015-11-15 00:54:51 +0000

    All optimizations done in Patch

--- P2/reader/primitives/Patch.js
+++ P2/reader/primitives/Patch.js
@@ -6,34 +6,9 @@ function Patch(scene, order, partsU, partsV, c_points) {
 	this.partsU = partsU;
 	this.partsV = partsV;
 	this.order = order;
-	this.controlVertexes = [];
-
-
-     this.surfaces = [];
-   	 this.translations = [];
-
-	// É preciso otimizar isto!!! 
-	if(order == 1){
-		this.controlVertexes[0] = [c_points[0],c_points[1]];
-		this.controlVertexes[1] = [c_points[2],c_points[3]];
-	}
-	else if(order == 2)
-	{
-		this.controlVertexes[0] = [c_points[0],c_points[1], c_points[2]];
-		this.controlVertexes[1] = [c_points[3],c_points[4], c_points[5]];
-		this.controlVertexes[2] = [c_points[6],c_points[7], c_points[8]];
-	}
-	else if(order == 3){
-	
-		this.controlVertexes[0] = [c_points[0],c_points[1], c_points[2] , c_points[3]];
-			//console.log(controlVertexes[0]);
-		this.controlVertexes[1] = [c_points[4],c_points[5], c_points[6], c_points[7]];
-		this.controlVertexes[2] = [c_points[8],c_points[9], c_points[10] , c_points[11]];
-		this.controlVertexes[3] = [c_points[12],c_points[13], c_points[14], c_points[15]];
-	}
+	this.controlVertexes = createMatrixCV(order, c_points);
 
 	this.initBuffers();
-
 }
 
 
@@ -77,4 +52,25 @@ function createKnot(order){
 			knot[j] = 1;
 	}
 	return knot;
+}
+
+function createMatrixCV(order, c_points){
+
+	var n = order+1;
+	var CV = [];
+	var index = 0;
+
+	for(var i = 0; i < n; i++)
+	{
+		var temp = [];
+		for(var j = 0; j < n; j++)
+		{
+			temp[j] = c_points[index];
+			index++;
+		}
+		CV[i] = temp;
+	}
+
+	return CV;
+
 }
